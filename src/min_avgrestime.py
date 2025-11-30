2366bd035dd30ed5f77d43cafe8660869d9a21ae
Nachat <nachatjatu@gmail.com>
2025-11-29 19:00:04 -0800

    all changes made since today's work session

--- src/min_avgrestime.py
+++ src/min_avgrestime.py
@@ -1,12 +1,13 @@
 import pickle
 
-import cvxpy as cvx
+# import cvxpy as cvx
 import geopandas as gpd
 import gurobipy as gp
 import networkx as nx
 import numpy as np
 import osmnx as ox
 from gurobipy import GRB
+import pandas as pd
 
 np.random.seed(0)
 
@@ -16,6 +17,29 @@ if __name__ == "__main__":
 
     # compute travel time
     G = ox.routing.add_edge_speeds(G)
+
+    # load CSV
+    accident_df = pd.read_csv('processed_data/accident_tables/wd_morning_accidents.csv').drop('date', axis=1)
+    congestion_df = pd.read_csv('processed_data/congestion_tables/wd_morning_congestion.csv').drop('date', axis=1)
+
+    to_drop = accident_df.isna().any(axis=1) | congestion_df.isna().any(axis=1)
+
+    accident_df = accident_df[~to_drop]
+    congestion_df = congestion_df[~to_drop]
+
+    # process accidents
+    accident_counts = (accident_df.to_numpy()).sum(axis=0)
+    accident_freqs = accident_counts / accident_counts.sum()
+    # process congestion
+    congestion_factors = congestion_df.mean(axis=0)
+    print(congestion_factors)
+
+    # impute travel time for each scenario
+    for idx, edge in enumerate(G.edges):
+        G[edge[0]][edge[1]][edge[2]][
+            "speed_kph"
+        ] *= (1 - congestion_factors[idx])
+
     G = ox.routing.add_edge_travel_times(G)
 
     # compute shortest time between depots and accidents
@@ -28,56 +52,55 @@ if __name__ == "__main__":
         for j, accident_id in enumerate(accident_nodes.index):
             A[i, j] = length[accident_id]
 
-    # probability of accident sites
-    P = np.random.rand(len(accident_nodes))
-    P /= np.linalg.norm(P)
+    # probability of accident sites and realization
+    P = accident_freqs
 
     # formulate coverage problem
     # problem variable
     num_depots = 10
 
-    # # Create a model using gurobi
-    # model = gp.Model("Accident coverage problem")
-
-    # # Create variables
-    # X = model.addMVar(
-    #     (len(hospital_nodes), len(accident_nodes)), vtype=GRB.BINARY, name="X"
-    # )
-    # y = model.addMVar(len(hospital_nodes), vtype=GRB.BINARY, name="y")
-
-    # # Set objective
-    # model.setObjective(((A * X) @ P).sum(), GRB.MINIMIZE)
-
-    # # Add constraint
-    # model.addConstr(X.sum(axis=0) == 1, "only one site")
-    # model.addConstr(
-    #     X.sum(axis=1) <= len(accident_nodes) * y, "only assign to open depot"
-    # )
-    # model.addConstr(y.sum() <= num_depots, "depot opening")
-
-    # # Optimize model
-    # model.optimize()
-
-    # print(f"Minimum average response time: {model.ObjVal:.2f}")
-    # print(y.X)
+    # Create a model using gurobi
+    model = gp.Model("Accident coverage problem")
 
     # Create variables
-    X = cvx.Variable((len(hospital_nodes), len(accident_nodes)), boolean=True)
-    y = cvx.Variable(len(hospital_nodes), boolean=True)
+    X = model.addMVar(
+        (len(hospital_nodes), len(accident_nodes)), vtype=GRB.BINARY, name="X"
+    )
+    y = model.addMVar(len(hospital_nodes), vtype=GRB.BINARY, name="y")
 
     # Set objective
-    obj = cvx.Minimize(cvx.sum(cvx.multiply(A, X) @ P))
+    model.setObjective(((A * X) @ P).sum(), GRB.MINIMIZE)
 
     # Add constraint
-    constraints = [
-        cvx.sum(X, axis=0) == 1,
-        X <= y[:, np.newaxis],
-        cvx.sum(y) <= num_depots,
-    ]
+    model.addConstr(X.sum(axis=0) == 1, "only one site")
+    model.addConstr(
+        X.sum(axis=1) <= len(accident_nodes) * y, "only assign to open depot"
+    )
+    model.addConstr(y.sum() <= num_depots, "depot opening")
 
     # Optimize model
-    prob = cvx.Problem(obj, constraints)
-    prob.solve(solver=cvx.SCIP, verbose=True)
+    model.optimize()
 
-    print(f"Minimum average response time: {prob.value:.2f}")
-    print(y.value)
+    print(f"Minimum average response time: {model.ObjVal:.2f}")
+    print(y.X)
+
+    # # Create variables
+    # X = cvx.Variable((len(hospital_nodes), len(accident_nodes)), boolean=True)
+    # y = cvx.Variable(len(hospital_nodes), boolean=True)
+
+    # # Set objective
+    # obj = cvx.Minimize(cvx.sum(cvx.multiply(A, X) @ P))
+
+    # # Add constraint
+    # constraints = [
+    #     cvx.sum(X, axis=0) == 1,
+    #     X <= y[:, np.newaxis],
+    #     cvx.sum(y) <= num_depots,
+    # ]
+
+    # # Optimize model
+    # prob = cvx.Problem(obj, constraints)
+    # prob.solve(solver=cvx.SCIP, verbose=True)
+
+    # print(f"Minimum average response time: {prob.value:.2f}")
+    # print(y.value)
--- src/min_worstrestime.py
+++ src/min_worstrestime.py
@@ -1,13 +1,19 @@
 import pickle
 from copy import deepcopy
 
-import cvxpy as cvx
+
+import pandas as pd
+# import cvxpy as cvx
 import geopandas as gpd
 import gurobipy as gp
 import networkx as nx
 import numpy as np
 import osmnx as ox
 from gurobipy import GRB
+from tqdm import tqdm
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 np.random.seed(0)
 
@@ -15,95 +21,108 @@ if __name__ == "__main__":
     with open("processed_data/bkk_augmented_graph.pickle", "rb") as f:
         G = pickle.load(f)
 
+    # load CSV
+    accident_df = pd.read_csv('processed_data/accident_tables/wd_morning_accidents.csv').drop('date', axis=1)
+    congestion_df = pd.read_csv('processed_data/congestion_tables/wd_morning_congestion.csv').drop('date', axis=1)
+
+    to_drop = accident_df.isna().any(axis=1) | congestion_df.isna().any(axis=1) | (accident_df == 0).all(axis=1)
+
+    accident_df = accident_df[~to_drop].head(30)
+    congestion_df = congestion_df[~to_drop].head(30)
+
+    # process accidents
+    accident_np = accident_df.to_numpy()
+    accident_counts = accident_np.sum(axis=1)
+    accident_freqs = accident_np / accident_counts[:, np.newaxis]
+
     # compute travel time
     G = ox.routing.add_edge_speeds(G)
-    G = ox.routing.add_edge_travel_times(G)
 
     # impute travel time for each scenario
     G_traffics = []
-    n_realizations = 10
-    for i in range(n_realizations):
+    for i, row in tqdm(congestion_df.iterrows()):
         G_traffics.append(deepcopy(G))
-        congestion_factor = np.random.choice(
-            [1.0, 1.25, 1.5], size=len(G_traffics[-1].edges)
-        )
-        for idx, edge in enumerate(G_traffics[-1].edges):
+
+        # impute travel time for each scenario
+        for j, edge in enumerate(G_traffics[-1].edges):
             G_traffics[-1][edge[0]][edge[1]][edge[2]][
-                "travel_time"
-            ] *= congestion_factor[idx]
+                "speed_kph"
+            ] *= (1 - row[j])
+
+        G_traffics[-1] = ox.routing.add_edge_travel_times(G_traffics[-1])
+
 
     # compute shortest time between depots and accidents
     A_traffics = []
-    for G_traffic in G_traffics:
+    for G_traffic in tqdm(G_traffics):
         nodes, edges = ox.graph_to_gdfs(G_traffic)
         hospital_nodes = nodes[nodes["vtype"] == "hosp"]
         accident_nodes = nodes[nodes["vtype"] == "accident"]
         A_traffics.append(np.zeros((len(hospital_nodes), len(accident_nodes))))
         for i, hosp_id in enumerate(hospital_nodes.index):
             length = nx.single_source_dijkstra_path_length(
-                G, hosp_id, weight="travel_time"
+                G_traffic, hosp_id, weight="travel_time"
             )
             for j, accident_id in enumerate(accident_nodes.index):
                 A_traffics[-1][i, j] = length[accident_id]
 
     # probability of accident sites and realization
-    P = np.random.rand(n_realizations, len(accident_nodes))
-    P /= np.linalg.norm(P, axis=1)[:, np.newaxis]
+    P = accident_freqs
 
     # formulate coverage problem
     # problem variable
     num_depots = 10
 
-    # # Create a model using gurobi
-    # model = gp.Model("Accident coverage problem")
-
-    # # Create variables
-    # X = model.addMVar(
-    #     (len(hospital_nodes), len(accident_nodes)), vtype=GRB.BINARY, name="X"
-    # )
-    # y = model.addMVar(len(hospital_nodes), vtype=GRB.BINARY, name="y")
-    # t = model.addVar()
-
-    # # Set objective
-    # model.setObjective(t, GRB.MINIMIZE)
-
-    # # Add constraint
-    # for i in range(n_realizations):
-    #     model.addConstr(
-    #         ((A_traffics[i] * X) @ P[i, :]).sum() <= t, f"realization {i + 1}"
-    #     )
-    # model.addConstr(X.sum(axis=0) == 1, "only one site")
-    # model.addConstr(
-    #     X.sum(axis=1) <= len(accident_nodes) * y, "only assign to open depot"
-    # )
-    # model.addConstr(y.sum() <= num_depots, "depot opening")
-
-    # # Optimize model
-    # model.optimize()
-
-    # print(f"Minimum worst case response time: {model.ObjVal:.2f}")
-    # print(y.X)
+    # Create a model using gurobi
+    model = gp.Model("Accident coverage problem")
 
     # Create variables
-    X = cvx.Variable((len(hospital_nodes), len(accident_nodes)), boolean=True)
-    y = cvx.Variable(len(hospital_nodes), boolean=True)
-    t = cvx.Variable()
+    X = model.addMVar(
+        (len(hospital_nodes), len(accident_nodes)), vtype=GRB.BINARY, name="X"
+    )
+    y = model.addMVar(len(hospital_nodes), vtype=GRB.BINARY, name="y")
+    t = model.addVar()
 
     # Set objective
-    obj = cvx.Minimize(t)
+    model.setObjective(t, GRB.MINIMIZE)
 
     # Add constraint
-    constraints = [
-        cvx.sum(X, axis=0) == 1,
-        X <= y[:, np.newaxis],
-        cvx.sum(y) <= num_depots,
-    ]
-    for i in range(n_realizations):
-        constraints.append(cvx.sum(cvx.multiply(A_traffics[i], X) @ P[i, :]) <= t)
+    for i in range(len(congestion_df)):
+        model.addConstr(
+            ((A_traffics[i] * X) @ P[i, :]).sum() <= t, f"realization {i + 1}"
+        )
+    model.addConstr(X.sum(axis=0) == 1, "only one site")
+    model.addConstr(
+        X.sum(axis=1) <= len(accident_nodes) * y, "only assign to open depot"
+    )
+    model.addConstr(y.sum() <= num_depots, "depot opening")
 
     # Optimize model
-    prob = cvx.Problem(obj, constraints)
-    prob.solve(solver=cvx.SCIP, verbose=True)
+    model.optimize()
 
-    print(f"Minimum average response time: {prob.value:.2f}")
-    print(y.value)
+    print(f"Minimum worst case response time: {model.ObjVal:.2f}")
+    print(y.X)
+
+    # # Create variables
+    # X = cvx.Variable((len(hospital_nodes), len(accident_nodes)), boolean=True)
+    # y = cvx.Variable(len(hospital_nodes), boolean=True)
+    # t = cvx.Variable()
+
+    # # Set objective
+    # obj = cvx.Minimize(t)
+
+    # # Add constraint
+    # constraints = [
+    #     cvx.sum(X, axis=0) == 1,
+    #     X <= y[:, np.newaxis],
+    #     cvx.sum(y) <= num_depots,
+    # ]
+    # for i in range(n_realizations):
+    #     constraints.append(cvx.sum(cvx.multiply(A_traffics[i], X) @ P[i, :]) <= t)
+
+    # # Optimize model
+    # prob = cvx.Problem(obj, constraints)
+    # prob.solve(solver=cvx.SCIP, verbose=True)
+
+    # print(f"Minimum average response time: {prob.value:.2f}")
+    # print(y.value)
